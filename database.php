2072e3412a3447bd726eeac8210fd5bb1001fda3
Paws <131801287+paws1234@users.noreply.github.com>
2024-05-10 23:30:31 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-
+//so wa ni na gamit kay tama tama man ko so uselses shit rani ash/lloyd tibook file
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
      header("Location: safe.php");
     
