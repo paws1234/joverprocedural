08bde34fb7cbbb3b3675cf6600610466200ddcd6
Paws <131801287+paws1234@users.noreply.github.com>
2024-05-10 23:42:35 +0800

    Update registration_process.php

--- registration_process.php
+++ registration_process.php
@@ -1,8 +1,10 @@
 <?php
+//para sure na post request rajud ang maka gamit sa kani na endpoint sa backend
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     header("Location: safe.php");
     exit;
 }
+//headers rani para security ash/lloyd
 header("Content-Security-Policy: default-src 'self'");
 header("X-Content-Type-Options: nosniff");
 header("X-Frame-Options: DENY");
@@ -11,16 +13,16 @@ session_start();
 session_regenerate_id();
 
 $response = array();
-
+//gi check ang payload sa csrf token kung na generate ba sa register na page chuy chuy rani
 if (!hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
     $response['error'] = "Oops! Something went wrong. Please try again later.";
     echo json_encode($response);
     exit;
 }
-
+//gi filter ang input shits para di ma sql injection og xss shits
 $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-
+//sabotable ra ang logic ubos sa pag register rani basic shits
 if (strlen($username) > 10) {
     $response['error'] = "Username should be maximum of 10 characters.";
     echo json_encode($response);
